b179ca2949abf9ac309e870e0d6d2e901949f773
Christian <christian.siegrist@open-circle.ch>
2026-09-03 10:30:31 +0200

    feat: simple erste landingpage version erstellt

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,10 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "zeigt die Startseite" do
+  test "zeigt die Startseite mit Upload-Feld" do
     get root_url
     assert_response :success
     assert_select "h1", "Lieferscheine"
+    assert_select "input[type=file]", 1
   end
 end
